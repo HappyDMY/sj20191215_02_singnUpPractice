7d4e2d959718ae41a03ea076fd04cd96986d79bb
jun <vagrantsj@gmail.com>
2019-12-15 12:32:33 +0900

    컬러 글래스를 이용해서 글씨색 변경

--- app/src/main/java/com/example/sj20191215_02_singnuppractice/SignUpActivity.kt
+++ app/src/main/java/com/example/sj20191215_02_singnuppractice/SignUpActivity.kt
@@ -1,6 +1,12 @@
 package com.example.sj20191215_02_singnuppractice
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import androidx.core.widget.addTextChangedListener
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : BaseActivity() {
 
@@ -9,9 +15,44 @@ class SignUpActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        setupEvents()
+        setValues()
     }
     override fun setupEvents() {
-        lastBackTime = 0
+//        pwEdt.addTextChangedListener(object : TextWatcher{
+//            override fun afterTextChanged(s: Editable?) {
+//            }
+//
+//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+//            }
+//
+//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+//                Log.d("입력된값" , s.toString())
+//            }
+//
+//        })
+
+
+//        pwEdt.addTextChangedListener {
+//            Log.d("입력된값", it.toString())
+//        }
+
+        pwEdt.addTextChangedListener {
+            Log.d("입력된값", it.toString())
+            val inputStr= it.toString()
+            if (inputStr.length==0){
+                pwStatusTxt.text ="버번입력하세요"
+                pwStatusTxt.setTextColor(Color.RED)
+            }else if (inputStr.length <8){
+                pwStatusTxt.text ="버번이 너무 짧음"
+                pwStatusTxt.setTextColor(Color.YELLOW)
+                pwStatusTxt.setTextColor(Color.parseColor("#FDA0EF"))
+            }else {
+                pwStatusTxt.text ="사용해도 좋은 비밀번호입니다."
+                pwStatusTxt.setTextColor(Color.GREEN)
+
+            }
+        }
     }
 
     override fun setValues() {
